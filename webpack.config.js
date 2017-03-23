c9f701820e6cdd93a98676ed213bd4b9cb90914b
Sergey Melentyev <sergey.melentyev@gmail.com>
2017-03-23 16:04:03 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require("webpack");
 module.exports = {
 	entry: "./src/index.js",
 	output: {
-		path: "dist/assets",
+		path: "./dist/assets",
 		filename: "bundle.js",
 		publicPath: "assets"
 	},
